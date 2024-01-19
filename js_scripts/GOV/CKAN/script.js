971020b3a49e5a55d74340bd7c550527e173d2f3
Stefanache Ion <ion.stefanache@yahoo.com>
2024-01-19 13:14:42 +0200

    Create script.js

---
+++ js_scripts/GOV/CKAN/script.js
@@ -0,0 +1,16 @@
+function parseResults(datasets) {
+    $list = $('#list')
+    for (var i in datasets) {
+        $list.append('<li>' + datasets[i] + '</li>')
+    }
+}
+
+function onButtonClick(event) {
+    $.ajax({
+        url: "http://data.gov.ro/api/3/action/package_list",
+        dataType: "jsonp",
+        success: function(response) {
+            parseResults(response.result)
+        },
+    })
+}
