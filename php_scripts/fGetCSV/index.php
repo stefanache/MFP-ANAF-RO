36e058281c88a79b7d3ad4c6e748ce0cb5f8f05a
Stefanache Ion <ion.stefanache@yahoo.com>
2024-04-08 16:20:10 +0300

    Update index.php

--- php_scripts/fGetCSV/index.php
+++ php_scripts/fGetCSV/index.php
@@ -15,18 +15,19 @@ if (file_exists($csv_file)) {
 //$url_csv_file="https://geospatialorg.github.io/date-contact-localitati/".$csv_fn.".git";
 //$url_csv_file="https://github.com/geospatialorg/date-contact-localitati/blob/main/".$csv_fn;
 //$url_csv_file="https://github.com/geospatialorg/date-contact-localitati/blob/527ca213334beba1c3e87ccbd3848fa405109c41/".$csv_fn;
+$url_csv_file="https://stefanache.github.io/MFP-ANAF-RO/php_scripts/fGetCSV/".$csv_fn;
 //echo "<br/>".$url_csv_file;
 //1.download file from url:
-/*
+
 if (!file_exists($csv_file)) {
 	$handle = fopen($url_csv_file, "r");
 	//echo("<br/>".$handle);
 	file_put_contents($csv_file, $handle )? 'The file '.$csv_file.' has been downloaded' : '*** Error downloading '.$csv_file.'!!';
 	fclose($handle);
 }else{
-	echo("*** The csv-file ".$csv_file." already exist!!!");
+	echo("<br/>*** The csv-file ".$csv_file." already exist!!!");
 }
-*/
+
 //2.read the csv-file downloaded and show me:
 $handle = fopen($csv_file, "r");
 if ($handle !== FALSE) {
