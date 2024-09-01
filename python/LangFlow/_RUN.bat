a8fccc4496ef3b508e31c620579586af9a7e7184
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-01 11:41:41 +0300

    Update _RUN.bat

--- python/LangFlow/_RUN.bat
+++ python/LangFlow/_RUN.bat
@@ -1,4 +1,4 @@
-M@echo off
+@echo off
 cls
 
 echo https://bytesupreme.online/crafting-no-code-local-rag-chatbots-with-langflow-and-ollama/
@@ -7,6 +7,7 @@ rem pip install streamlit
 REM pip install langflow
 rem pip install langchain-community
 
+echo -dati refresh in browser daca server-ul langflow nu a fost incarcat inca!
 start Chrome http://127.0.0.1:7860
 langflow run
 
