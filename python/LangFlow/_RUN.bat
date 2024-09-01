edf4325cff3dc1a3d4ebdb1cc2f8ad0e5728dbc0
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-01 11:25:41 +0300

    Add files via upload

---
+++ python/LangFlow/_RUN.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo https://bytesupreme.online/crafting-no-code-local-rag-chatbots-with-langflow-and-ollama/
+rem pip install streamlit
+rem pip install langflow
+rem pip install langchain-community
+start Chrome http://127.0.0.1:7860
+langflow run
+pause
