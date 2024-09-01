99e6cd384bd9c718d9793e8809f8ed2f3dc1e322
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-01 11:42:03 +0300

    Update _RUN.bat

--- python/LangFlow/_RUN.bat
+++ python/LangFlow/_RUN.bat
@@ -7,7 +7,7 @@ rem pip install streamlit
 REM pip install langflow
 rem pip install langchain-community
 
-echo -dati refresh in browser daca server-ul langflow nu a fost incarcat inca!
+echo -dati refresh in browser daca server-ul Langflow nu a fost incarcat inca!
 start Chrome http://127.0.0.1:7860
 langflow run
 
