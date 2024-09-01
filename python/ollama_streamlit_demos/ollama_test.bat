6a5fefb7182154057a5fee4c900c922393f72aa2
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-01 14:23:37 +0300

    Update ollama_test.bat

--- python/ollama_streamlit_demos/ollama_test.bat
+++ python/ollama_streamlit_demos/ollama_test.bat
@@ -1,5 +1,9 @@
 @echo off
 cls
 curl http://127.0.0.1:11434/|more
-curl http://localhost:11434/v1/models |more
+
+echo for linux replace ^^ with \
+curl http://127.0.0.1:11434/v1/models 			^
+    -H "Content-Type: application/json"
+
 pause	
