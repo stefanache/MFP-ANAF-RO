a057f2abd16b14f911ad9c6acd32aa67dd4cefca
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-08 14:50:47 +0300

    Update _2.curl_cli.bat

--- python/llamafile_llava/_2.curl_cli.bat
+++ python/llamafile_llava/_2.curl_cli.bat
@@ -8,6 +8,6 @@ echo https://github.com/Mozilla-Ocho/llamafile
 curl http://localhost:8080/v1/chat/completions ^
 -H "Content-Type: application/json" ^
 -H "Authorization: Bearer no-key" ^
--d @cli_rest_llama_cpp.txt | python3 -c @py_json_dump.txt |more
+-d @cli_rest_llama_cpp.txt | python -c @py_json_dump.txt |more
 
 pause
