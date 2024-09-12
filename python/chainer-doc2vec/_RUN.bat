62b0f142b102ea38f863dc91e79910ba0d9d0a80
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-12 11:50:17 +0300

    Update _RUN.bat

--- python/chainer-doc2vec/_RUN.bat
+++ python/chainer-doc2vec/_RUN.bat
@@ -4,7 +4,7 @@ echo https://github.com/monthly-hack/chainer-doc2vec/tree/master
 
 rem pip install chainer
 
-echo must be patient...if have not more resources like GPU....
+echo TRAIN: must be patient...if have not more resources like GPU....
 
 python train_doc2vec.py --model dm-dbow |more
 
