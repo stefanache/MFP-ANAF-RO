207b5cd8e51ec46d9bff2fea884e6c8277b65664
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-12 11:50:00 +0300

    Update _Test.bat

--- python/chainer-doc2vec/_Test.bat
+++ python/chainer-doc2vec/_Test.bat
@@ -2,7 +2,7 @@
 cls
 echo https://github.com/monthly-hack/chainer-doc2vec/tree/master
 
-echo must be patient...if have not more resources like GPU....
+echo TEST: must be patient...if have not more resources like GPU....
 
 python search.py < input.txt |more
 
