97a5d3520094a78755c58513eaf329e58be9391e
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-14 19:40:39 +0300

    Add files via upload

---
+++ php_scripts/CRUD_API/_RUN.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+echo https://github.com/stefanache/MFP-ANAF-RO/blob/main/php_scripts/CRUD_API/ReadMe.md
+
+echo Asteptati putin si apoi Dati Refresh daca browserul nu incarca continutul....
+start Chrome   http://localhost:8080/api.php/records/posts/1 |more
+
+echo minimizati fereastra dar nu o inchideti pt a lasa webserver-ul incorporat al PHP-ului, in functiune...
+php -S localhost:8080 |more
+  
+pause 
