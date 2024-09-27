07f6ab7e09ea3d1b5b9fb1c170f1afd6f7844a37
Stefanache Ion <ion.stefanache@yahoo.com>
2024-09-27 12:57:08 +0300

    Update create_mysql.sql

--- php_scripts/CRUD_API/tests/fixtures/create_mysql.sql
+++ php_scripts/CRUD_API/tests/fixtures/create_mysql.sql
@@ -1,9 +1,9 @@
 -- cat create_mysql.sql | sudo mysql
 --
 DROP USER IF EXISTS 'php-crud-api'@'localhost';
-DROP DATABASE IF EXISTS `php-crud-api`;
+DROP DATABASE IF EXISTS `php_crud_api_db`;
 --
-CREATE DATABASE `php-crud-api` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE `php_crud_api_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER 'php-crud-api'@'localhost' IDENTIFIED BY 'php-crud-api';
-GRANT ALL PRIVILEGES ON `php-crud-api`.* TO 'php-crud-api'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `php_crud_api_db`.* TO 'php-crud-api'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
