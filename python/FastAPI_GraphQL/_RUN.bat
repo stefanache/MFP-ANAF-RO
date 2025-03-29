8e7ab35c042e7ef6c3271d12b332138a0a6f6a84
Stefanache Ion <ion.stefanache@yahoo.com>
2025-03-29 17:15:55 +0200

    Update _RUN.bat

--- python/FastAPI_GraphQL/_RUN.bat
+++ python/FastAPI_GraphQL/_RUN.bat
@@ -1,6 +1,8 @@
 @echo off
 cls
 
+echo https://www.geeksforgeeks.org/install-fastapi-and-run-your-first-fastapi-server-on-windows/
+
 REM python -m pip install fastapi uvicorn | more
 REM python -m pip list |more
 
@@ -8,5 +10,7 @@ REM notepad main.py
 REM type main.py
 
 start Chrome http://127.0.0.1:8000/
+
 python -m uvicorn main:app --reload
+
 pause
